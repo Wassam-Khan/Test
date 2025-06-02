89d32c87f863c5bbe93573c42123ed7d167faee2
Wassam-Khan <wassamk44@gmail.com>
2025-06-02 07:44:14 -0700

    test3

--- test.py
+++ test.py
@@ -6,10 +6,3 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split 
 from sklearn.svm import SVC 
 from sklearn.metrics import classification_report, accuracy_score
-
-plt.scatter(X, y, color='darkorange',
-            label='data')
-plt.plot(X, y_pred, color='cornflowerblue',
-         label='prediction')
-plt.legend()
-plt.show()

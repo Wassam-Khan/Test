0fcd8c48db9dad5b48bbc343313370de821a6fcd
Wassam-Khan <wassamk44@gmail.com>
2025-06-02 07:56:06 -0700

    test7

--- test.py
+++ test.py
@@ -7,10 +7,5 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC 
 from sklearn.metrics import classification_report, accuracy_score
 
-import pandas as pd 
-# Load dataset 
-df = pd.read_csv("C:\\Users\\hp\\OneDrive\\Desktop\\spam.csv", encoding="latin-1") 
-df = df[['v1', 'v2']]  # Retaining relevant columns 
-df.columns = ['label', 'message'] 
 # Display first few rows 
 df.head() 

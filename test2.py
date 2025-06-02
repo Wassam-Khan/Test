a9692ee4210645212e36ce84385a5ff503796330
Wassam-Khan <wassamk44@gmail.com>
2025-06-02 07:52:06 -0700

    test5

---
+++ test2.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+from sklearn import datasets 
+from sklearn.model_selection import train_test_split 
+from sklearn.svm import SVC 
+from sklearn.metrics import classification_report, accuracy_score
